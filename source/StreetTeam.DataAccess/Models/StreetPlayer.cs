b4465def4108a036eaca8eb9cde0bf62c8788559
kolyb <kolybenko@mail.ru>
2024-06-17 22:52:18 +0300

    Create StreetPlayer

---
+++ source/StreetTeam.DataAccess/Models/StreetPlayer.cs
@@ -0,0 +1,11 @@
+﻿namespace StreetTeam.DataAccess.Models
+{
+    public class StreetPlayer
+    {
+        public int? IdPlayer { get; set; }
+
+        public string? NickNamePlayer { get; set; }
+
+        public string? PhoneNumberPlayer { get; set; }
+    }
+}
