6b62fef2fabe498d9cbcb1cfbea846d3d00743c0
kolyb <kolybenko@mail.ru>
2024-06-17 23:26:36 +0300

    Create PlaceGame

---
+++ source/StreetTeam.DataAccess/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿namespace StreetTeam.DataAccess.Models
+{
+    public class Event
+    {
+        public int? IdEvent { get; set; }
+
+        public string? NameEvent { get; set; }
+
+        public DateTime? DateTimeEvent { get; set; }
+
+        public int?  PlaceGameId { get; set; }
+
+        public int?  StreetPlayerId { get; set; }
+    }
+}
---
+++ source/StreetTeam.DataAccess/Models/PlaceGame.cs
@@ -0,0 +1,11 @@
+﻿namespace StreetTeam.DataAccess.Models
+{
+    public class PlaceGame
+    {
+        public int? IdPlaceGame { get; set; }
+
+        public string? NamePlaceGame { get; set; }
+
+        public string? AddressPlaceGame { get; set; }
+    }
+}
