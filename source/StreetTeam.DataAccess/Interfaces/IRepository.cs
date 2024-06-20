77da54286aa46d70bc5ef1d7debc465ef7d9a3b1
kolyb <kolybenko@mail.ru>
2024-06-20 23:28:20 +0300

    Create IRepository.cs

---
+++ source/StreetTeam.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace StreetTeam.DataAccess.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task CreateAsync (T item);
+
+        Task DeleteAsync (T item);
+        
+        Task UpdateAsync (T item);
+
+        Task<T> GetByIdAsync (int? id);
+
+        Task<IEnumerable<T>> GetAllAsync ();
+
+    }
+}
