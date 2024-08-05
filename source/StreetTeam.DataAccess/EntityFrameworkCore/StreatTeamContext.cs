8b4755e226fa30e119c0aa022b889b6226501827
kolyb <kolybenko@mail.ru>
2024-08-05 23:32:42 +0300

    Create StreatTeamContext

---
+++ source/StreetTeam.DataAccess/EntityFrameworkCore/StreatTeamContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StreetTeam.DataAccess.Models;
+
+namespace StreetTeam.DataAccess.EntityFrameworkCore
+{
+    public class StreatTeamContext : IdentityDbContext
+    { 
+        public StreatTeamContext() { }
+
+        public StreatTeamContext(DbContextOptions<StreatTeamContext> options) : base(options) { }
+
+        public DbSet<Event> Events { get; set; }
+
+        public DbSet<PlaceGame> PlaceGames { get; set; }
+
+        public DbSet<StreetPlayer> StreetPlayers { get; set; }
+    }
+}
